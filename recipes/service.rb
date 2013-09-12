ac8ea2d9eeae567fadc5d25decf01e45ac409615
Phil Cohen <github@phlippers.net>
2013-09-12 16:50:11 -0700

    only start the service if not already running

--- recipes/service.rb
+++ recipes/service.rb
@@ -5,7 +5,10 @@
 # Copyright 2013, Phil Cohen <github@phlippers.net>
 
 
+service_actions = [:enable]
+service_actions << :start unless system("pgrep nginx")
+
 service "nginx" do
   supports status: true, restart: true, reload: true
-  action [:enable, :start]
+  action service_actions
 end
