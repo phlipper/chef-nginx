ec2be0ddf351876652bac31fd79ccb22a8f38853
Phil Cohen <github@phlippers.net>
2013-11-06 11:34:02 -0800

    refactor repository sources, add phusion as option

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,26 +4,15 @@
 #
 # Copyright 2013, Phil Cohen <github@phlippers.net>
 
-if node["nginx"]["repository"] == "official"
 
-  apt_repository "nginx" do
-    uri "http://nginx.org/packages/#{node["platform"]}"
-    distribution node["lsb"]["codename"]
-    components ["nginx"]
-    action :add
-    key "http://nginx.org/keys/nginx_signing.key"
-  end
+repo = node["nginx"]["repository_sources"].fetch(node["nginx"]["repository"])
 
-elsif node["nginx"]["repository"] == "ppa"
-
-  apt_repository "nginx" do
-    uri "http://ppa.launchpad.net/nginx/stable/ubuntu"
-    distribution node["lsb"]["codename"]
-    components ["main"]
-    action :add
-    keyserver "keyserver.ubuntu.com"
-    key "C300EE8C"
-    deb_src true
-  end
-
+apt_repository "nginx" do
+  uri repo["uri"]
+  distribution repo["distribution"]
+  components repo["components"]
+  key repo["key"]
+  keyserver repo["keyserver"] if repo["keyserver"]
+  deb_src repo["deb_src"]
+  action :add
 end
