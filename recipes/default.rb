ef208b2a6537067f9c312acedc5a4a402eef7f64
Arvid Björkström <arvid@bjorkstrom.se>
2014-06-02 00:41:14 +0200

    The nxensite and nxdissite scripts should be in their own receipt - and be run from the server receipt, not the default (and add chef spec tests)

--- recipes/default.rb
+++ recipes/default.rb
@@ -14,17 +14,3 @@ apt_repository "nginx" do
   deb_src repo["deb_src"]
   action :add
 end
-
-template "#{node['nginx']['bin_dir']}/nxensite" do
-  source "nxensite.erb"
-  owner "root"
-  group "root"
-  mode "0755"
-  not_if { ::File.exists?("#{node['nginx']['bin_dir']}/nxensite") }
-end
-
-execute "Link #{node['nginx']['bin_dir']}/nxdissite" do
-  command "ln -s #{node['nginx']['bin_dir']}/nxensite #{node['nginx']['bin_dir']}/nxdissite"
-  only_if { ::File.exists?("#{node['nginx']['bin_dir']}/nxensite") }
-  not_if { ::File.symlink?("#{node['nginx']['bin_dir']}/nxdissite") }
-end
---
+++ spec/enabledisablesite_spec.rb
@@ -0,0 +1,27 @@
+require "spec_helper"
+
+describe "nginx::enabledisablesite" do
+  let(:chef_run) do
+    ChefSpec::Runner.new.converge(described_recipe)
+  end
+
+  it "creates the `nxensite` script" do
+    ::File.stub(:exists?).with(anything).and_call_original
+    ::File.stub(:exists?).with('/usr/sbin/nxensite').and_return(false)
+
+    expect(chef_run).to create_template("/usr/sbin/nxensite").with(
+      source: "nxensite.erb",
+      owner: "root",
+      group: "root",
+      mode:  "0755"
+    )
+  end
+
+  it "creates the `nxdissite` link" do
+    ::File.stub(:exists?).with(anything).and_call_original
+    ::File.stub(:exists?).with('/usr/sbin/nxensite').and_return(true)
+    ::File.stub(:symlink?).with('/usr/sbin/nxdissite').and_return(false)
+
+    expect(chef_run).to run_execute("ln -s /usr/sbin/nxensite /usr/sbin/nxdissite")
+  end
+end
