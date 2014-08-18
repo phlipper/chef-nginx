c21505aa3f095c7a547201aab7b63e5b762046bd
Phil Cohen <github@phlippers.net>
2014-08-18 12:16:49 -0700

    fix `LocalJumpError` due to early return

--- providers/site.rb
+++ providers/site.rb
@@ -10,87 +10,88 @@ end
 action :create do
   if @current_resource.exists
     Chef::Log.info "#{@new_resource} already exists - nothing to do."
-    return
-  end
-  converge_by("Create configuration file for #{@new_resource}") do
-    template "#{node["nginx"]["dir"]}/sites-available/#{new_resource.name}" do
-      source new_resource.templatesource
-      cookbook new_resource.templatecookbook
-      owner "root"
-      group "root"
-      mode "0644"
-      variables(
-        name: new_resource.name,
-        listen: new_resource.listen,
-        host: new_resource.host,
-        root: new_resource.root,
-        index: new_resource.index,
-        slashlocation: new_resource.slashlocation,
-        phpfpm: new_resource.phpfpm,
-        accesslog: new_resource.accesslog
-      )
-      not_if do
-        ::File.exist?(
-          "#{node["nginx"]["dir"]}/sites-available/#{new_resource.name}"
+  else
+    converge_by("Create configuration file for #{@new_resource}") do
+      template "#{node["nginx"]["dir"]}/sites-available/#{new_resource.name}" do
+        source new_resource.templatesource
+        cookbook new_resource.templatecookbook
+        owner "root"
+        group "root"
+        mode "0644"
+        variables(
+          name: new_resource.name,
+          listen: new_resource.listen,
+          host: new_resource.host,
+          root: new_resource.root,
+          index: new_resource.index,
+          slashlocation: new_resource.slashlocation,
+          phpfpm: new_resource.phpfpm,
+          accesslog: new_resource.accesslog
         )
+        not_if do
+          ::File.exist?(
+            "#{node["nginx"]["dir"]}/sites-available/#{new_resource.name}"
+          )
+        end
       end
     end
   end
 end
 
 action :delete do
-  unless @current_resource.exists
-    Chef::Log.info "#{@new_resource} doesn't exists - nothing to do."
-  end
-
-  converge_by("Disable and remove configuration file for #{@new_resource}") do
-    nginx_site new_resource.name do
-      action :disable
-    end
-    file "#{node["nginx"]["dir"]}/sites-available/#{new_resource.name}" do
-      only_if do
-        ::File.exist?(
-          "#{node["nginx"]["dir"]}/sites-available/#{new_resource.name}"
-        )
+  if @current_resource.exists
+    converge_by("Disable and remove configuration file for #{@new_resource}") do
+      nginx_site new_resource.name do
+        action :disable
       end
-      action :delete
+
+      file "#{node["nginx"]["dir"]}/sites-available/#{new_resource.name}" do
+        action :delete
+        only_if do
+          ::File.exist?(
+            "#{node["nginx"]["dir"]}/sites-available/#{new_resource.name}"
+          )
+        end
+      end
     end
+  else
+    Chef::Log.info "#{@new_resource} doesn't exists - nothing to do."
   end
 end
 
 action :enable do
-  unless @current_resource.exists
-    Chef::Log.info "#{@new_resource} doesn't exists - nothing to do."
-  end
-
-  converge_by("Enable #{@new_resource} configuration and restart nginx") do
-    execute "nxensite #{new_resource.name}" do
-      command "#{node["nginx"]["bin_dir"]}/nxensite #{new_resource.name}"
-      not_if do
-        ::File.symlink?(
-          "#{node["nginx"]["dir"]}/sites-enabled/#{new_resource.name}"
-        )
+  if @current_resource.exists
+    converge_by("Enable #{@new_resource} configuration and restart nginx") do
+      execute "nxensite #{new_resource.name}" do
+        command "#{node["nginx"]["bin_dir"]}/nxensite #{new_resource.name}"
+        not_if do
+          ::File.symlink?(
+            "#{node["nginx"]["dir"]}/sites-enabled/#{new_resource.name}"
+          )
+        end
+        notifies :reload, "service[nginx]"
       end
-      notifies :reload, "service[nginx]"
     end
+  else
+    Chef::Log.info "#{@new_resource} doesn't exists - nothing to do."
   end
 end
 
 action :disable do
-  unless @current_resource.exists
-    Chef::Log.info "#{@new_resource} doesn't exists - nothing to do."
-  end
-
-  converge_by("Disable #{@new_resource} if enabled and restart nginx") do
-    execute "nxdissite #{new_resource.name}" do
-      command "#{node["nginx"]["bin_dir"]}/nxdissite #{new_resource.name}"
-      only_if do
-        ::File.symlink?(
-          "#{node["nginx"]["dir"]}/sites-enabled/#{new_resource.name}"
-        )
+  if @current_resource.exists
+    converge_by("Disable #{@new_resource} if enabled and restart nginx") do
+      execute "nxdissite #{new_resource.name}" do
+        command "#{node["nginx"]["bin_dir"]}/nxdissite #{new_resource.name}"
+        only_if do
+          ::File.symlink?(
+            "#{node["nginx"]["dir"]}/sites-enabled/#{new_resource.name}"
+          )
+        end
+        notifies :reload, "service[nginx]"
       end
-      notifies :reload, "service[nginx]"
     end
+  else
+    Chef::Log.info "#{@new_resource} doesn't exists - nothing to do."
   end
 end
 
