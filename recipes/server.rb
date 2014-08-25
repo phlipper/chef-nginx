2ff15aa1fe3e0c5e4adce73f6f6c1e97e38d8600
Phil Cohen <github@phlippers.net>
2014-08-25 16:06:06 -0700

    ensure `nginx-common` doesn’t conflict with config

--- recipes/server.rb
+++ recipes/server.rb
@@ -5,6 +5,12 @@
 
 include_recipe "nginx"
 
+# ensure config files don't get trampled by chef
+package "nginx-common" do
+  options %(-o Dpkg::Options::="--force-confdef")
+  only_if { %w[ppa phusion].include?(node["nginx"]["repository"]) }
+end
+
 package node["nginx"]["package_name"] do
   version node["nginx"]["version"]
 end
