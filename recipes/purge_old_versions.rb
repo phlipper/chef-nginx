4dddefce93d29016d17043bff7ecefd977f76d52
Mike Heijmans <parabuzzle@gmail.com>
2015-03-14 11:07:54 -0700

    Fix styling issues

--- recipes/purge_old_versions.rb
+++ recipes/purge_old_versions.rb
@@ -3,9 +3,20 @@
 # Recipe:: purge_old_versions
 #
 
-%w(nginx nginx-light nginx-naxsi nginx-common passenger nginx-extras passenger-enterprise nginx-full).each do |pkg|
+package_list = [
+  "nginx",
+  "nginx-light",
+  "nginx-naxsi",
+  "nginx-common",
+  "passenger",
+  "nginx-extras",
+  "passenger-enterprise",
+  "nginx-full"
+]
+
+package_list.each do |pkg|
   apt_package pkg do
     action :purge
-    only_if { node['nginx']['purge_old'] }
+    only_if { node["nginx"]["purge_old"] }
   end
 end
